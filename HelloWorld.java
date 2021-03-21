c70c8d6bda7e809955737c35f4594ac4a39a6fcc
Zachary McCain <zacmmcca@gmail.com>
2021-03-21 09:18:26 -0500

    Updated the world.

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,13 +1,15 @@
 public class HelloWorld {
   int worldHeight;
   int worldWidth;
-  public HelloWorld(int height, int width) {
+  String worldName;
+  public HelloWorld(String name, int height, int width) {
+    worldName = name;
     worldHeight = height;
     worldWidth = width;
   }
   public static void main(String[] args) {
-    HelloWorld myWorld = new HelloWorld(5, 5);
-    System.out.println(myWorld.worldHeight);
-    System.out.println(myWorld.worldWidth);
+    HelloWorld myWorld = new HelloWorld("James's World", 5, 5);
+    System.out.println("World Name: " + myWorld.worldName);
+    System.out.println("World Size: " + myWorld.worldHeight + " x " + myWorld.worldWidth);
   }
 }
