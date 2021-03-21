768411728889498ce64a13fbefce169dbd7c9805
Zachary McCain <zacmmcca@gmail.com>
2021-03-21 08:09:21 -0500

    Playing around with class structure.

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,13 @@
 public class HelloWorld {
+  int worldHeight;
+  int worldWidth;
+  public HelloWorld(int height, int width) {
+    worldHeight = height;
+    worldWidth = width;
+  }
   public static void main(String[] args) {
-    System.out.println("Hello World!");
+    HelloWorld myWorld = new HelloWorld(5, 5);
+    System.out.println(myWorld.worldHeight);
+    System.out.println(myWorld.worldWidth);
   }
 }
